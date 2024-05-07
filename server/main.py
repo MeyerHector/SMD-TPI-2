e29c5a402e266fdc054ac1f507e0203caf6417e0
Meyer <meyer-hector@hotmail.com>
2024-05-06 23:11:46 -0300

    Se corrigio problema del cors del servidor

--- server/main.py
+++ server/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from controllers import get_category, get_country, get_rating, get_realese
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir solicitudes desde cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
+# Tus rutas van aquí
 
 @app.get("/df_rating")
 async def route_get_rating():
